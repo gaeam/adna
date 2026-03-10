a90a3face4c56e404344858e02178de617d8361e
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-11 02:26:04 +0800

    Fix typo in sample names file argument

--- data/calling/random_calling.sh
+++ data/calling/random_calling.sh
@@ -16,6 +16,6 @@ awk '{print $2}' bam_indiv.map > indiv.list
 samtools mpileup -B -q30 -Q30 -s -O -R -l /mnt/data3/kexin_li/Goat/Panel/panel.bed \
     -f /mnt/data3/Genomes/Ovis_Capra_genome_zehui_210817/Capra/bwa-0.5.10 \
     -b bam.list | \
-pileupCaller --randomHaploid --sampleNamesFile indiv.list \
+pileupCaller --randomHaploid --sampleNameFile indiv.list \
     --samplePopName POP1 -f /mnt/data3/kexin_li/Goat/Panel/isec_auto.snp \
     -e own6.a103
