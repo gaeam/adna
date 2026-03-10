635d5075ce3c11931545caf294e364ca3f006577
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-11 00:44:57 +0800

    Modify callsnp.sh for SNP calling process
    Updated the script to prepare a positions file for pileupCaller and adjusted samtools mpileup parameters.

--- data/calling/callsnp.sh
+++ data/calling/callsnp.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-samtools mpileup -B -q30 -Q20 -s -O -l <panel.bed> -R -f <ref.fa> <input.bam> \
-| pileupCaller --randomHaploid \
+# 将 pos 文件整理为 pileupCaller 需要的格式（0-based）
+# pos 文件是从 bcftools isec 的结果 txt 文件中提取的
+# If you use a positions-file, it should either contain positions (0-based) or a bed file (see samtools manual for details). 
+awk '{print $1"\t"$2-1"\t"$2}' isec_auto.pos > panel.bed
 
---sampleNameFile low_sample.list --samplePopName POP1 -f <panel.snp> -e <PREFIX.pileupCaller>
---sampleNames NAME1,NAME2
+# 准备待 call snp 的样本序号与样本名的映射文件
+
+
+# random calling
+samtools mpileup -B -q30 -Q30 -s -O -R -l panel.bed \
+    -f /mnt/data3/Genomes/Ovis_Capra_genome_zehui_210817/Capra/bwa-0.5.10 \
+    -b bam.list | \
+pileupCaller --randomHaploid --sampleNamesFile sample.list \
+    --samplePopName POP1 -f isec_auto.snp \
+    -e own6.a103
