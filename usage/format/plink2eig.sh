656460c8e04700ff2d2de82afe4c4edc8310e49c
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-10 13:37:32 +0800

    Update plink2eig.sh

--- usage/format/plink2eig.sh
+++ usage/format/plink2eig.sh
@@ -4,3 +4,5 @@ source /home/kexin_li/miniforge3/etc/profile.d/conda.sh
 conda activate bioenv
 
 /home/kexin_li/EIG-8.0.0/bin/convertf -p par.PED.EIGENSTRAT
+
+# 这种方式生成的 EIGENSTRAT 文件中的 ind 文件里的群体信息需要自己编辑
