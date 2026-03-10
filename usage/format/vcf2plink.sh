5f6cb434f7d286cb13f5ed0f1cfb666ce8a6b51c
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-10 14:02:29 +0800

    Update vcf2plink.sh

--- usage/format/vcf2plink.sh
+++ usage/format/vcf2plink.sh
@@ -6,3 +6,7 @@
 	--keep-allele-order \
 	--make-bed \
 	--out GGVD_232
+
+# 重命名 bim 文件中的位点
+awk '{print$1"\t"$1"_"$4"\t"$3"\t"$4"\t"$5"\t"$6}' isec_auto.bim > isec_auto.new.bim
+mv isec_auto.new.bim isec_auto.bim
