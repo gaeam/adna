f11c784d4f9c2ceee872d8ddbead4498ed63debf
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-11 00:57:39 +0800

    Update and rename callsnp.sh to random_calling.sh

--- data/calling/random_calling.sh
+++ data/calling/random_calling.sh
@@ -5,13 +5,17 @@
 # If you use a positions-file, it should either contain positions (0-based) or a bed file (see samtools manual for details). 
 awk '{print $1"\t"$2-1"\t"$2}' isec_auto.pos > panel.bed
 
-# 准备待 call snp 的样本序号与样本名的映射文件
+# 准备待 call snp 的样本序号与样本名的映射文件 bam_indiv.map
+# /path/A.bam  SampleA
+# /path/B.bam  SampleB
 
+awk '{print $1}' bam_indiv.map > bam.list
+awk '{print $2}' bam_indiv.map > indiv.list
 
 # random calling
 samtools mpileup -B -q30 -Q30 -s -O -R -l panel.bed \
     -f /mnt/data3/Genomes/Ovis_Capra_genome_zehui_210817/Capra/bwa-0.5.10 \
     -b bam.list | \
-pileupCaller --randomHaploid --sampleNamesFile sample.list \
+pileupCaller --randomHaploid --sampleNamesFile indiv.list \
     --samplePopName POP1 -f isec_auto.snp \
     -e own6.a103
