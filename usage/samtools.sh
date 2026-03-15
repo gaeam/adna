3097dc88b1c766a65547ab1da2fa8bac51b96b3d
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-15 16:18:11 +0800

    Update samtools.sh

--- usage/samtools.sh
+++ usage/samtools.sh
@@ -8,3 +8,6 @@ samtools reheader cleaned_header.sam YJL02G.autosomal.bam > YJL02G.autosomal.reh
 
 # 提取常染色体
 samtools view -b SAMBG15.merged.uniq.rescaled.bam 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 > SAMBG15.merged.uniq.rescaled.autosomal.bam
+
+# 从 fasta 参考基因组中提取常染色体
+samtools faidx ARS1.fna 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 > ARS1_auto.fna
