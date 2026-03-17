cbbbfc399b6cd1c84b3f581169b79a509c91dfb0
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-17 15:32:03 +0800

    Update bcftools.sh

--- usage/bcftools.sh
+++ usage/bcftools.sh
@@ -7,6 +7,9 @@ bcftools annotate --rename-chrs chr_map.txt vargoats_snps_1372_20230313_auto.vcf
 # 查询个体名
 bcftools query -l vcf.gz
 
+# 查询染色体名
+bcftools query -f '%CHROM\n' input.vcf.gz | sort -u
+
 # 查询 SNP 数量
 bcftools +counts <vcf.gz 文件路径> > <结果 txt 文件路径>
 bcftools index -n Goat.sample232.SNP_auto.vcf.gz # 只在有索引文件时生效
