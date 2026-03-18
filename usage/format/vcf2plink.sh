7fc9849303449e1c08403efb32fc6db82a9648f6
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-18 14:17:44 +0800

    Update vcf2plink.sh

--- usage/format/vcf2plink.sh
+++ usage/format/vcf2plink.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# --double-id 是因为有些样本名里有 `_`，加入这个参数之后就会直接把完整样本名复制给FID和IID
 /home/kexin_li/plink-1.9/plink --vcf Goat.sample232.SNP_auto.vcf.gz \
 	--chr-set 29 \
 	--allow-no-sex \
 	--keep-allele-order \
 	--make-bed \
+	--double-id \
 	--out GGVD_232
 
 # 重命名 bim 文件中的位点
