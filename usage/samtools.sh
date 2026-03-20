60bb501be0e406763599f3340f42f6bde48c315a
gaeam <155247923+gaeam@users.noreply.github.com>
2026-03-20 19:34:37 +0800

    Update samtools.sh

--- usage/samtools.sh
+++ usage/samtools.sh
@@ -11,3 +11,6 @@ samtools view -b SAMBG15.merged.uniq.rescaled.bam 1 2 3 4 5 6 7 8 9 10 11 12 13 
 
 # 从 fasta 参考基因组中提取常染色体
 samtools faidx ARS1.fna 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 > ARS1_auto.fna
+
+# 统计样本比对情况
+samtools flagstat input.bam
