5b4ec72510a7ad854c4bb406ffcd376703db302e
gaeam <155247923+gaeam@users.noreply.github.com>
2026-05-22 16:59:42 +0800

    Add comments to LD_pruning.sh for clarity
    Added comments explaining parameters for LD pruning.

--- ADMIXTURE/LD_pruning.sh
+++ ADMIXTURE/LD_pruning.sh
@@ -7,3 +7,9 @@
     --allow-no-sex \
     --keep-allele-order \
     --out /Users/likexin/Desktop/cattle/admixture/modern216_ld.ancient121.own5.outgroup2
+
+# --indep-pairwise 50 5 0.5 = 滑动窗口 50 SNP，每次移动 5 SNP，如果 2 个 SNP 的 r^2 > 0.5，就剔除其中 1 个
+# --allow-no-sex = 忽略 sex 列
+# --keep-allele-order = 保持等位基因顺序不翻转
+# prune.in = 保留下来的独立 SNP 列表
+# prune.out = 被剔除的 SNP 列表
