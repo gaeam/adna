7fc5f17b28c655c7fafb41969990ab1b43675ef1
gaeam <155247923+gaeam@users.noreply.github.com>
2026-05-22 18:33:18 +0800

    Update SNP_filtering.sh

--- ADMIXTURE/SNP_filtering.sh
+++ ADMIXTURE/SNP_filtering.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 /home/kexin_li/plink-1.9/plink \
-  --bfile /Users/likexin/Desktop/cattle/snp.10k/modern216_ld.ancient121.own5.outgroup2 \
-  --extract /Users/likexin/Desktop/cattle/admixture/modern216_ld.ancient121.own5.outgroup2.prune.in \
+  --bfile /mnt/data3/kexin_li/Goat/Analysis/PCA/GGVD+ancient/own6.agoat93.abezoar6.GGVD226 \
+  --extract own6.agoat93.abezoar6.GGVD226.prune.in \
   --make-bed \
   --chr-set 29 \
   --allow-no-sex \
   --keep-allele-order \
-  --out /Users/likexin/Desktop/cattle/admixture/modern216_ld.ancient121.own5.outgroup2.prune
+  --out own6.agoat93.abezoar6.GGVD226.pruned
 
 # --extract = 保留 prune.in 文件列出的 SNP
