68f6d35dcde2a60254889e64d59ab2f97b888d5c
gaeam <155247923+gaeam@users.noreply.github.com>
2026-06-15 01:13:45 +0800

    Update kpPlotBAMDensity.R

--- data/quality_control/kpPlotBAMDensity.R
+++ data/quality_control/kpPlotBAMDensity.R
@@ -12,5 +12,15 @@
 library(karyoploteR)
 ARS1 <- toGRanges("D:/Downloads/ARS1.txt)
 kp <- plotKaryotype(genome = ARS1)
-kpPlotBAMDensity(kp, data = "D:/Downloads/NWU/L0139.uniq.mask2.bam", col "navy")
-kpAddMainTitle(kp, "L0139.uniq.mask2.bam")
+
+# window.size defaults to 1e6, 1Mb
+# 只有 plotKaryotype 和 kpPlotBAMDensity 需要保存（也就是复制给一个参数），kpAddMainTitle 和 kpAxis 是绘图函数，不需要保存
+kp_L0139 <- kpPlotBAMDensity(kp, data = "D:/Downloads/NWU/L0139.uniq.mask2.bam", col "navy", normalize = TRUE, window.size = 5e5) 
+kpAddMainTitle(kp_L0139, "L0139.uniq.mask2.bam")
+
+# 给 reads density 图添加纵坐标轴
+# maxD = kp_L0139$latest.plot$computed.values$max.density
+# kpAxis(kp_L0139, ymin = 0, ymax = maxD*1.2, cex = 0.6)
+
+# 如果有一个图层画错了，只能关闭设备重新绘图
+# dev.off()
